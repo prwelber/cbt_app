777e21eca89d4d1b466186cde6023020cac16030
Philip Welber <prwelber@gmail.com>
2015-12-22 11:14:18 -0500

    gitignore

--- seeds.sql
+++ seeds.sql
@@ -1,1 +1,3 @@
 INSERT INTO users values(1, 'shmee', 'password', 'cooper', 'welber', 'dog@bark.com', 'Date');
+INSERT INTO users values(2, 'jack', 'password', 'Jack', 'Welber', 'jwelber@yahoo.com', 'Date');
+INSERT INTO users values(3, 'james', 'password', 'James', 'Lafferty', 'jazzlobsters@gmail.com', 'Date');
